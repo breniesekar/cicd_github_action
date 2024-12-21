49ed06f82e5fcb5e235f16c481d1e06758fd79cb
breniesekar <breniesekar@gmail.com>
2024-12-21 08:22:23 +0530

    unittest_corrected

--- test/test_operation.py
+++ test/test_operation.py
@@ -6,7 +6,7 @@ def test_add():
 
 def test_sub():
     assert sub(5,3)==2
-    assert add(4,3)==1
-    assert add(3,3)==0
-    assert add(2,3)==-1
+    assert sub(4,3)==1
+    assert sub(3,3)==0
+    assert sub(2,3)==-1
 
